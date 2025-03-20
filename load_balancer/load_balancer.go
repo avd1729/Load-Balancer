885c7ce4bc66cf000a51e6b060458c0f9bbe3701
Aravind <aravind19112003@gmail.com>
2025-03-20 18:36:55 +0530

    Add functionality to check the health of the server

--- load_balancer/load_balancer.go
+++ load_balancer/load_balancer.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 var servers = []string{
 	"http://127.0.0.1:5001",
@@ -10,9 +14,24 @@ var servers = []string{
 
 var currServer = 0
 
-func server() string {
+func getServer() string {
 	currServer = (currServer + 1) % len(servers)
 	return servers[currServer]
+}
+
+func checkServerHealth(server string) bool {
+	client := http.Client{
+		Timeout: 3 * time.Second,
+	}
+
+	resp, err := client.Get(server)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusOK
 }
 
 func main() {
