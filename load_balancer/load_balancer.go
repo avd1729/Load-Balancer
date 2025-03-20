dcbba711dc6e087b9544624ba27131fbc588f280
Aravind <aravind19112003@gmail.com>
2025-03-20 18:30:31 +0530

    Add functionality to rotate servers

--- load_balancer/load_balancer.go
+++ load_balancer/load_balancer.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var servers = []string{
+	"http://127.0.0.1:5001",
+	"http://127.0.0.1:5002",
+	"http://127.0.0.1:5003",
+}
+
+var currServer = 0
+
+func server() string {
+	currServer = (currServer + 1) % len(servers)
+	return servers[currServer]
+}
+
+func main() {
+	fmt.Println(servers)
 }
